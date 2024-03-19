35ac5e92e304a5d34030ba39b53f1349ed83b030
Yobuligo <hoffmannpeter82@gmail.com>
2024-03-19 11:13:24 +0100

    Add useTimer

--- src/hooks/OnTickHandler.ts
+++
@@ -1,1 +0,0 @@
-export type OnTickHandler = (remainingSeconds: number) => void;
--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,114 +1,48 @@
-import { useMemo, useState } from "react";
-import { ITimer } from "./ITimer";
-import { OnFinishHandler } from "./OnFinishHandler";
-import { OnTickHandler } from "./OnTickHandler";
+import { useEffect, useMemo, useState } from "react";
+import { OnFinishHandler } from "../core/services/timer/OnFinishHandler";
+import { OnRemainingSecondsChangeHandler } from "../core/services/timer/OnRemainingSecondsChangeHandler";
+import { Timer } from "../core/services/timer/Timer";
 
-interface ITimerState {
-  /**
-   * Returns if the timer is currently running
-   */
-  isRunning: boolean;
-
-  /**
-   * Returns if the timer was already started. Even so it is currently paused, it was started before.
-   */
-  started: boolean;
-}
-
-export const useTimer = (seconds: number): ITimer => {
+export const useTimer = (seconds: number) => {
   const [remainingSeconds, setRemainingSeconds] = useState(seconds);
+  const timer = useMemo(() => new Timer(seconds), [seconds]);
 
-  const onFinishHandlers: OnFinishHandler[] = useMemo(() => [], []);
+  useEffect(() => {
+    return () => {
+      timer.destruct();
+    };
+  }, [timer]);
 
-  const onTickHandlers: OnTickHandler[] = useMemo(() => [], []);
+  const isRunning = timer.isRunning;
 
-  let timerState: ITimerState = useMemo(
-    () => ({ isRunning: false, started: false }),
-    []
-  );
-
-  const onStartTimer = (endTime: Date) => {
-    // Do not restart timer, if it is already running
-    if (timerState.isRunning) {
-      return;
-    }
-    timerState.isRunning = true;
-    timerState.started = true;
-    startTimer(endTime, timerState);
-  };
-
-  const onFinishTimer = () => {
-    setRemainingSeconds(0);
-    timerState.isRunning = false;
-    timerState.started = false;
-    onFinishHandlers.forEach((handler) => handler());
-    return;
-  };
-
-  const startTimer = (endTime: Date, timerState: ITimerState) => {
-    // Stop timer, if it was stopped from outside
-    if (!timerState.isRunning) {
-      return;
-    }
-
-    setTimeout(() => {
-      const now = new Date();
-      const remainingSeconds = Math.round(
-        (endTime.getTime() - now.getTime()) / 1000
-      );
-
-      if (remainingSeconds <= 0) {
-        onFinishTimer();
-        return;
-      }
-
-      onTickHandlers.forEach((handler) => handler(remainingSeconds));
-      setRemainingSeconds(remainingSeconds);
-      startTimer(endTime, timerState);
-    }, 200);
-  };
-
-  const isPaused = timerState.started && !timerState.isRunning;
+  const isPaused = timer.isPaused;
 
   const onFinish = (handler: OnFinishHandler) => {
-    onFinishHandlers.push(handler);
-    return () => {
-      const index = onFinishHandlers.findIndex((item) => item === handler);
-      if (index !== -1) {
-        onFinishHandlers.splice(index, 1);
-      }
-    };
+    return timer.onFinish(handler);
   };
 
-  const onTick = (handler: OnTickHandler) => {
-    onTickHandlers.push(handler);
-    return () => {
-      const index = onTickHandlers.findIndex((item) => item === handler);
-      if (index !== -1) {
-        onTickHandlers.splice(index, 1);
-      }
-    };
-  };
-
-  const stop = () => {
-    timerState.isRunning = false;
+  const onTick = (handler: OnRemainingSecondsChangeHandler) => {
+    return timer.onTick(handler);
   };
 
   const reset = () => {
-    timerState.isRunning = false;
-    timerState.started = false;
-    setRemainingSeconds(seconds);
+    timer.reset();
   };
 
   const start = () => {
-    const endTime = new Date();
-    endTime.setSeconds(endTime.getSeconds() + remainingSeconds);
-    onStartTimer(endTime);
+    timer.start();
+    timer.onRemainingSecondsChange((remainingSeconds) =>
+      setRemainingSeconds(remainingSeconds)
+    );
+  };
+
+  const stop = () => {
+    timer.stop();
   };
 
   return {
     remainingSeconds,
-    isRunning: timerState.isRunning,
+    isRunning,
     isPaused,
     onFinish,
     onTick,
