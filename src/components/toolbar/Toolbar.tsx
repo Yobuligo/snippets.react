dfecf3be9d6a7933585cdc8afe1171646cf8b977
Yobuligo <hoffmannpeter82@gmail.com>
2024-07-29 23:27:46 +0200

    Add Toolbar

---
+++ src/components/toolbar/Toolbar.tsx
@@ -0,0 +1,6 @@
+import { IToolbarProps } from "./IToolbarProps";
+import styles from "./Toolbar.module.scss";
+
+export const Toolbar: React.FC<IToolbarProps> = (props) => {
+  return <div className={styles.toolbar}>{props.children}</div>;
+};
