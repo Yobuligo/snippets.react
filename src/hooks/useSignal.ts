c97bfcce834ca8fdf9b1b4ce04edd13fa0596de1
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-01 11:14:44 +0200

    Correct useSignal

--- src/hooks/useSignal.ts
+++ src/hooks/useSignal.ts
@@ -2,7 +2,10 @@ import { useState } from "react";
 import { ISignal } from "../services/signal/ISignal";
 import { Signal } from "../services/signal/Signal";
 
-export const useSignal = () => {
+export const useSignal = (): [
+  signal: ISignal | undefined,
+  triggerSignal: () => void
+] => {
   const [signal, setSignal] = useState<ISignal | undefined>(undefined);
   const triggerSignal = () => setSignal(new Signal());
   return [signal, triggerSignal];
