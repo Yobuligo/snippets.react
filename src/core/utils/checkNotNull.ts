2ceb2b422a29b9b609876eab7ed0594c397a4926
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-08 12:25:05 +0200

    Extend checkNotNull and checkNull

--- src/core/utils/checkNotNull.ts
+++ src/core/utils/checkNotNull.ts
@@ -1,9 +1,8 @@
-import { isNull } from "./isNull";
-
-export const checkNotNull = (value: any) => {
-  if (isNull(value)) {
+export const checkNotNull = <T>(value: T): NonNullable<T> => {
+  if (value === undefined || value === null) {
     throw new Error(
       `Error while checking value. Value was expected to be not null.`
     );
   }
+  return value;
 };
--- src/core/utils/checkNull.ts
+++ src/core/utils/checkNull.ts
@@ -1,9 +1,10 @@
 import { isNotNull } from "./isNotNull";
 
-export const checkNull = (value: any) => {
+export const checkNull = (value: any): undefined => {
   if (isNotNull(value)) {
     throw new Error(
       `Error while checking value. Value was expected to be null.`
     );
   }
+  return undefined;
 };
