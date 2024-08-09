dead2232de858b497bf480e93ea955edc6067a12
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-09 10:20:10 +0200

    Base to-methods on disassemble

--- src/core/services/date/DateTime.ts
+++ src/core/services/date/DateTime.ts
@@ -79,122 +79,6 @@ export class DateTime {
   }
 
   /**
-   * Converts the given {@link date} to a string in format yyyy-mmm-dd hh:mm:ss.fff
-   */
-  static format(date: Date): string {
-    const [dateString, timeString] = this.toLocalISOString(date).split("T");
-    return `${dateString} ${timeString}`;
-  }
-
-  static formatDate(date: Date): string {
-    const [year, month, day] = this.toDate(date).split("-");
-    return `${day}.${month}.${year}`;
-  }
-
-  /**
-   * Returns an instance of Date of the current time
-   */
-  static now(): Date {
-    return new Date();
-  }
-
-  /**
-   * Subtracts the given {@link duration} from {@link date} and returns a new date instance.
-   */
-  static subtract(date: Date, duration: Duration): Date;
-  /**
-   * Calculates the difference between the given {@link later} and {@link earlier} dates and returns the result as duration.
-   */
-  static subtract(later: Date, earlier: Date): Duration;
-  static subtract(first: Date, second: unknown): any {
-    if (second instanceof Date) {
-      const msec =
-        this.toDateInstance(first).getTime() -
-        this.toDateInstance(second).getTime();
-      return new Duration(msec);
-    }
-
-    if (second instanceof Duration) {
-      return new Date(
-        this.toDateInstance(first).getTime() - second.totalMilliseconds
-      );
-    }
-  }
-
-  /**
-   * Converts the given {@link date} to a string in format yyyy-mmm-dd
-   */
-  static toDate(date: Date): string {
-    const [dateString] = this.toLocalISOString(date).split("T");
-    return dateString;
-  }
-
-  /**
-   * Extracts and returns the days of the given {@link date}.
-   */
-  static toDay(date: Date): number {
-    const [, , day] = this.toDate(date).split("-");
-    return parseInt(day);
-  }
-
-  /**
-   * Extracts and returns the hours of the given {@link date}.
-   */
-  static toHours(date: Date): number {
-    const [hours] = this.toTime(date).split(":");
-    return parseInt(hours);
-  }
-
-  /**
-   * Converts the given {@link date} to a string in format yyyy-mm-ddThh:mm:ss.fff for the current time zone.
-   */
-  static toLocalISOString(date: Date): string {
-    if (date instanceof Date) {
-      const year = date.getFullYear();
-      const month = String(date.getMonth() + 1).padStart(2, "0");
-      const day = String(date.getDate()).padStart(2, "0");
-      const hours = String(date.getHours()).padStart(2, "0");
-      const minutes = String(date.getMinutes()).padStart(2, "0");
-      const seconds = String(date.getSeconds()).padStart(2, "0");
-      const milliseconds = String(date.getMilliseconds()).padStart(3, "0");
-      return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}`;
-    }
-    return date;
-  }
-
-  /**
-   * Extracts and returns the minutes of the given {@link date}.
-   */
-  static toMinutes(date: Date): number {
-    const [, minutes] = this.toTime(date).split(":");
-    return parseInt(minutes);
-  }
-
-  /**
-   * Extracts and returns the months of the given {@link date}.
-   */
-  static toMonth(date: Date): number {
-    const [, month] = this.toDate(date).split("-");
-    return parseInt(month);
-  }
-
-  /**
-   * Extracts and returns the minutes of the given {@link date}.
-   */
-  static toSeconds(date: Date): number {
-    const [, , seconds] = this.toTime(date).split(":");
-    return parseInt(seconds);
-  }
-
-  /**
-   * Converts the given {@link date} to a string in format hh:mm:ss.fff.
-   */
-  static toTime(date: Date): string {
-    const [, timeString] = this.toLocalISOString(date).split("T");
-    return timeString;
-  }
-
-  /**
    * Disassembles the given {@link date} into its components.
    */
   static disassemble(
@@ -241,11 +125,92 @@ export class DateTime {
   }
 
   /**
+   * Returns an instance of Date of the current time
+   */
+  static now(): Date {
+    return new Date();
+  }
+
+  /**
+   * Subtracts the given {@link duration} from {@link date} and returns a new date instance.
+   */
+  static subtract(date: Date, duration: Duration): Date;
+  /**
+   * Calculates the difference between the given {@link later} and {@link earlier} dates and returns the result as duration.
+   */
+  static subtract(later: Date, earlier: Date): Duration;
+  static subtract(first: Date, second: unknown): any {
+    if (second instanceof Date) {
+      const msec =
+        this.toDateInstance(first).getTime() -
+        this.toDateInstance(second).getTime();
+      return new Duration(msec);
+    }
+
+    if (second instanceof Duration) {
+      return new Date(
+        this.toDateInstance(first).getTime() - second.totalMilliseconds
+      );
+    }
+  }
+
+  /**
+   * Extracts and returns the days of the given {@link date}.
+   */
+  static toDay(date: Date): number {
+    return this.disassemble(date)[2];
+  }
+
+  /**
+   * Extracts and returns the hours of the given {@link date}.
+   */
+  static toHours(date: Date): number {
+    return this.disassemble(date)[3];
+  }
+
+  /**
+   * Converts the given {@link date} to a string in format yyyy-mm-ddThh:mm:ss.fff for the current time zone.
+   */
+  static toLocalISOString(date: Date): string {
+    if (date instanceof Date) {
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      const day = String(date.getDate()).padStart(2, "0");
+      const hours = String(date.getHours()).padStart(2, "0");
+      const minutes = String(date.getMinutes()).padStart(2, "0");
+      const seconds = String(date.getSeconds()).padStart(2, "0");
+      const milliseconds = String(date.getMilliseconds()).padStart(3, "0");
+      return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}`;
+    }
+    return date;
+  }
+
+  /**
+   * Extracts and returns the minutes of the given {@link date}.
+   */
+  static toMinutes(date: Date): number {
+    return this.disassemble(date)[4];
+  }
+
+  /**
+   * Extracts and returns the months of the given {@link date}.
+   */
+  static toMonth(date: Date): number {
+    return this.disassemble(date)[1];
+  }
+
+  /**
+   * Extracts and returns the seconds of the given {@link date}.
+   */
+  static toSeconds(date: Date): number {
+    return this.disassemble(date)[5];
+  }
+
+  /**
    * Extracts and returns the years of the given {@link date}.
    */
   static toYear(date: Date): number {
-    const [year] = this.toDate(date).split("-");
-    return parseInt(year);
+    return this.disassemble(date)[0];
   }
 
   /**
