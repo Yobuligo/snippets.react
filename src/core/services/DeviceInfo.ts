22b3c483130dceec2211ab99426b09ae7f903447
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-13 21:55:17 +0200

    Add DeviceInfo

---
+++ src/core/services/DeviceInfo.ts
@@ -0,0 +1,24 @@
+/**
+ * This class is responsible for providing information about the underlying device
+ */
+export class DeviceInfo {
+  /**
+   * Derives the platform from navigator.userAgent
+   */
+  get platform(): string {
+    const userAgent = navigator.userAgent;
+    if (userAgent.includes("Windows NT")) {
+      return "Windows";
+    } else if (userAgent.includes("Mac OS X")) {
+      return "MacOS";
+    } else if (userAgent.includes("Linux")) {
+      return "Linux";
+    } else if (userAgent.includes("Android")) {
+      return "Android";
+    } else if (userAgent.includes("like Mac OS X")) {
+      return "iOS";
+    } else {
+      return "Unknown";
+    }
+  }
+}
