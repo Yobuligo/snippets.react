0561b1d010274684eccd299a84ce19ea4e4647b3
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-16 11:23:08 +0200

    Add useRenderWeekday

---
+++ src/hooks/useRenderWeekday.ts
@@ -0,0 +1,31 @@
+import { IllegalArgumentError } from "../core/errors/IllegalArgumentError";
+import { Weekday } from "../core/types/Weekday";
+import { texts } from "./useTranslation/texts";
+import { useTranslation } from "./useTranslation/useTranslation";
+
+export const useRenderWeekday = () => {
+  const { t } = useTranslation();
+
+  const render = (weekday: Weekday): string => {
+    switch (weekday) {
+      case Weekday.MONDAY:
+        return t(texts.general.weekdays.monday);
+      case Weekday.TUESDAY:
+        return t(texts.general.weekdays.tuesday);
+      case Weekday.WEDNESDAY:
+        return t(texts.general.weekdays.wednesday);
+      case Weekday.THURSDAY:
+        return t(texts.general.weekdays.thursday);
+      case Weekday.FRIDAY:
+        return t(texts.general.weekdays.friday);
+      case Weekday.SATURDAY:
+        return t(texts.general.weekdays.saturday);
+      case Weekday.SUNDAY:
+        return t(texts.general.weekdays.sunday);
+      default:
+        throw new IllegalArgumentError();
+    }
+  };
+
+  return { render };
+};
