78af4627f4b35e160ec40e044db6a288e019bb33
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-21 10:48:04 +0200

    Extend useRenderWeekday by short version

--- src/hooks/useRenderWeekday.ts
+++ src/hooks/useRenderWeekday.ts
@@ -6,24 +6,45 @@ import { useTranslation } from "./useTranslation/useTranslation";
 export const useRenderWeekday = () => {
   const { t } = useTranslation();
 
-  const render = (weekday: Weekday): string => {
-    switch (weekday) {
-      case Weekday.MONDAY:
-        return t(texts.general.weekdays.monday);
-      case Weekday.TUESDAY:
-        return t(texts.general.weekdays.tuesday);
-      case Weekday.WEDNESDAY:
-        return t(texts.general.weekdays.wednesday);
-      case Weekday.THURSDAY:
-        return t(texts.general.weekdays.thursday);
-      case Weekday.FRIDAY:
-        return t(texts.general.weekdays.friday);
-      case Weekday.SATURDAY:
-        return t(texts.general.weekdays.saturday);
-      case Weekday.SUNDAY:
-        return t(texts.general.weekdays.sunday);
-      default:
-        throw new IllegalArgumentError();
+  const render = (weekday: Weekday, short?: boolean): string => {
+    if (short) {
+      switch (weekday) {
+        case Weekday.MONDAY:
+          return t(texts.general.weekdays.mondayShort);
+        case Weekday.TUESDAY:
+          return t(texts.general.weekdays.tuesdayShort);
+        case Weekday.WEDNESDAY:
+          return t(texts.general.weekdays.wednesdayShort);
+        case Weekday.THURSDAY:
+          return t(texts.general.weekdays.thursdayShort);
+        case Weekday.FRIDAY:
+          return t(texts.general.weekdays.fridayShort);
+        case Weekday.SATURDAY:
+          return t(texts.general.weekdays.saturdayShort);
+        case Weekday.SUNDAY:
+          return t(texts.general.weekdays.sundayShort);
+        default:
+          throw new IllegalArgumentError();
+      }
+    } else {
+      switch (weekday) {
+        case Weekday.MONDAY:
+          return t(texts.general.weekdays.monday);
+        case Weekday.TUESDAY:
+          return t(texts.general.weekdays.tuesday);
+        case Weekday.WEDNESDAY:
+          return t(texts.general.weekdays.wednesday);
+        case Weekday.THURSDAY:
+          return t(texts.general.weekdays.thursday);
+        case Weekday.FRIDAY:
+          return t(texts.general.weekdays.friday);
+        case Weekday.SATURDAY:
+          return t(texts.general.weekdays.saturday);
+        case Weekday.SUNDAY:
+          return t(texts.general.weekdays.sunday);
+        default:
+          throw new IllegalArgumentError();
+      }
     }
   };
 
