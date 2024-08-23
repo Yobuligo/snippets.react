af5074a4a0ec529ec36807acfb88c777e6414b98
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-23 09:56:35 +0200

    Extend Collapse by introducing outer container

--- src/components/collapse/Collapse.tsx
+++ src/components/collapse/Collapse.tsx
@@ -13,13 +13,15 @@ export const Collapse: React.FC<ICollapseProps> = (props) => {
     });
 
   return (
-    <button className={styles.collapse} onClick={onToggleCollapsed}>
+    <div className={styles.collapse} onClick={onToggleCollapsed}>
       {props.title && <div className={props.titleClassName}>{props.title}</div>}
-      {props.collapsed ? (
-        <ExpandedIcon className={componentStyles.clickableIcon} />
-      ) : (
-        <CollapsedIcon className={componentStyles.clickableIcon} />
-      )}
-    </button>
+      <button className={styles.button}>
+        {props.collapsed ? (
+          <ExpandedIcon className={componentStyles.clickableIcon} />
+        ) : (
+          <CollapsedIcon className={componentStyles.clickableIcon} />
+        )}
+      </button>
+    </div>
   );
 };
