c597c147b027a16226e03eb9417d54893e6393da
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-28 10:20:41 +0200

    Introduce UrlParamsExtenderRegistry

---
+++ src/api/core/UrlParamsExtenderRegistry.ts
@@ -0,0 +1,6 @@
+import { UrlParamsExtenderConstructors } from "../../lib/urlParamsExtender/types/UrlParamsExtenderRegistry";
+
+/**
+ * This list contains all UrlParamsExtenders that should extend a REST call url.
+ */
+export const UrlParamsExtenderRegistry: UrlParamsExtenderConstructors = [];
