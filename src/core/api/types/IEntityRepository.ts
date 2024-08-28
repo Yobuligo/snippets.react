d700b59aaec02c4d91a7bf4854a47a6a6bcf2205
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-28 14:15:21 +0200

    Introduce IRepository and IEntityRepository

---
+++ src/core/api/types/IEntityRepository.ts
@@ -0,0 +1,41 @@
+import { IEntity } from "./IEntity";
+import { IEntityDetails } from "./IEntityDetails";
+import { IEntitySubset } from "./IEntitySubset";
+import { IRepository } from "./IRepository";
+
+/**
+ * An implementation of this interface represents a repository that provides CRUD operations to entities of type {@link TEntity}.
+ */
+export interface IEntityRepository<TEntity extends IEntity>
+  extends IRepository<TEntity> {
+  deleteById(id: string): Promise<boolean>;
+
+  findAll<K extends keyof TEntity>(
+    fields: K[]
+  ): Promise<IEntitySubset<TEntity, K>[]>;
+  findAll(): Promise<TEntity[]>;
+
+  findById<K extends keyof TEntity>(
+    id: string,
+    fields: K[]
+  ): Promise<IEntitySubset<TEntity, K> | undefined>;
+  findById(id: string): Promise<TEntity | undefined>;
+
+  insert<K extends keyof TEntity>(
+    entity: IEntityDetails<TEntity>,
+    fields: K[]
+  ): Promise<IEntitySubset<TEntity, K>>;
+  insert(entity: IEntityDetails<TEntity>): Promise<TEntity>;
+
+  update<K extends keyof TEntity>(
+    entity: TEntity,
+    fields: K[]
+  ): Promise<IEntitySubset<TEntity, K>>;
+  update(entity: TEntity): Promise<TEntity>;
+
+  updateAll<K extends keyof TEntity>(
+    entities: TEntity[],
+    fields: K[]
+  ): Promise<IEntitySubset<TEntity, K>[]>;
+  updateAll(entities: TEntity[]): Promise<TEntity[]>;
+}
---
+++ src/core/api/types/IRepository.ts
@@ -0,0 +1,4 @@
+/**
+ * An implementation of this interface represents a repository to handle data of type {@link T}.
+ */
+export interface IRepository<T> {}
