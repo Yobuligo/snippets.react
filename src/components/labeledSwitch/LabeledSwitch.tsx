f1351ee69ae318ed4ce5e2177264478ca29a47c2
Yobuligo <hoffmannpeter82@gmail.com>
2024-11-28 17:41:01 +0100

    Update LabeledSwitch

--- src/components/labeledSwitch/LabeledSwitch.tsx
+++ src/components/labeledSwitch/LabeledSwitch.tsx
@@ -2,20 +2,13 @@ import { useId } from "react";
 import { LabeledElement } from "../labeledElement/LabeledElement";
 import { Switch } from "../switch/Switch";
 import { ILabeledSwitchProps } from "./ILabeledSwitchProps";
-import styles from "./LabeledInput.module.scss";
 
 export const LabeledSwitch: React.FC<ILabeledSwitchProps> = (props) => {
   const id = useId();
 
   return (
     <LabeledElement elementId={id} label={props.label}>
-      <div className={styles.inputContainer}>
-        <Switch
-          checked={props.checked}
-          onChange={props.onChange}
-          width="4rem"
-        />
-      </div>
+      <Switch checked={props.checked} onChange={props.onChange} width="4rem" />
     </LabeledElement>
   );
 };
