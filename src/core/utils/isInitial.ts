1a615760c4a3150dc4a986e484cd3a46bc52668b
Yobuligo <hoffmannpeter82@gmail.com>
2024-12-13 15:48:12 +0100

    Add documentation for isInitial and isNotInitial

--- src/core/utils/isInitial.ts
+++ src/core/utils/isInitial.ts
@@ -1,5 +1,14 @@
 import { isNull } from "./isNull";
 
+/**
+ * This function returns true if the given {@link value} is initial otherwise false.
+ * Returns true if:
+ * 1. {@link value} is an array and has no entries
+ * 2. {@link value} is of type string with a length of 0
+ * 3. {@link value} is of type number and the value is 0
+ * 4. {@link value} is an object which is null
+ * 5. {@link value} is undefined
+ */
 export const isInitial = (value: any): boolean => {
   if (Array.isArray(value)) {
     return value.length === 0;
--- src/core/utils/isNotInitial.ts
+++ src/core/utils/isNotInitial.ts
@@ -1,3 +1,13 @@
 import { isInitial } from "./isInitial";
 
+/**
+ * This function returns true if the given {@link value} is not initial otherwise false.
+ * 
+ * Returns true if:
+ * 1. {@link value} is an array and having at least one entry
+ * 2. {@link value} is of type string and having at least a length of 1
+ * 3. {@link value} is of type number and the value is greater or smaller 0
+ * 4. {@link value} is an object which is not null
+ * 5. {@link value} is defined / not undefined
+ */
 export const isNotInitial = (value: any): boolean => !isInitial(value);
