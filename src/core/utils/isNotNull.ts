7ea1b84b1e510e0031acd5b1cb8ce82e151138ba
Yobuligo <hoffmannpeter82@gmail.com>
2024-12-13 15:50:56 +0100

    Add documentation for isNotNull and isNull

--- src/core/utils/isNotNull.ts
+++ src/core/utils/isNotNull.ts
@@ -1,3 +1,6 @@
+/**
+ * Returns true if the given {@link value} is not undefined and not null, otherwise false.
+ */
 export const isNotNull = (value: any): boolean => {
   return value !== undefined && value !== null;
 };
--- src/core/utils/isNull.ts
+++ src/core/utils/isNull.ts
@@ -1,3 +1,6 @@
+/**
+ * Returns true if the given {@link value} is undefined or null, otherwise false.
+ */
 export const isNull = (value: any): boolean => {
   return value === undefined || value === null;
 };
