f1e486df69c4e881fcaf55beac90d546e4c4cfb4
Yobuligo <hoffmannpeter82@gmail.com>
2026-02-23 12:59:56 +0100

    Add docu

--- src/libs/backend/sequelize/core/types/IHaveStaticModel.ts
+++ src/libs/backend/sequelize/core/types/IHaveStaticModel.ts
@@ -1,4 +1,7 @@
 import { IHaveStaticAssociate } from "./IHaveStaticAssociate";
 import { IHaveStaticInitModel } from "./IHaveStaticInitModel";
 
+/**
+ * Represents a model that can be initialized via static methods initModel and associate.
+ */
 export type IHaveStaticModel = IHaveStaticInitModel & IHaveStaticAssociate;
